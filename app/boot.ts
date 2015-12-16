59b1ef7c33052c99da11310ed8aaef80e0e615d3
Christine Wilks <crissxross@gmail.com>
2015-12-16 13:17:55 +0000

    Use default HTML 5 URL style  LocationStrategy

--- app/boot.ts
+++ app/boot.ts
@@ -1,9 +1,7 @@
 import {bootstrap} from 'angular2/platform/browser';
-import {provide} from 'angular2/core';
-import {ROUTER_PROVIDERS, HashLocationStrategy, LocationStrategy} from 'angular2/router';
+import {ROUTER_PROVIDERS} from 'angular2/router';
 import {AppComponent} from './app.component';
 
 bootstrap(AppComponent, [
 	ROUTER_PROVIDERS,
-	provide(LocationStrategy, {useClass: HashLocationStrategy})
 ]);
